a56d89afb7fa2c0da99d8d78b0d02da328c72cfc
gharjare <gharjarechetan@gmail.com>
2023-02-11 05:55:49 +0530

    [chetan gharjare] initial

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace GenericsProblem
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Generics problem");
+
+        }
+    }
+}
